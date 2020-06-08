efd00596c4c95dea9640e1ad8e00414c32046a9e
Tyler Manion <relytmanion@gmail.com>
2020-06-08 16:15:03 -0500

    updated resume

--- js/resume.js
+++ js/resume.js
@@ -1,81 +1,91 @@
 (function($) {
-  "use strict"; // Start of use strict
+	"use strict"; // Start of use strict
 
-  // Smooth scrolling using jQuery easing
-  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
-    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-      if (target.length) {
-        $('html, body').animate({
-          scrollTop: (target.offset().top)
-        }, 1000, "easeInOutExpo");
-        return false;
-      }
-    }
-  });
+	// Smooth scrolling using jQuery easing
+	$('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+		if (
+			location.pathname.replace(/^\//, "") ==
+				this.pathname.replace(/^\//, "") &&
+			location.hostname == this.hostname
+		) {
+			var target = $(this.hash);
+			target = target.length
+				? target
+				: $("[name=" + this.hash.slice(1) + "]");
+			if (target.length) {
+				$("html, body").animate(
+					{
+						scrollTop: target.offset().top
+					},
+					1000,
+					"easeInOutExpo"
+				);
+				return false;
+			}
+		}
+	});
 
-  // Closes responsive menu when a scroll trigger link is clicked
-  $('.js-scroll-trigger').click(function() {
-    $('.navbar-collapse').collapse('hide');
-  });
+	// Closes responsive menu when a scroll trigger link is clicked
+	$(".js-scroll-trigger").click(function() {
+		$(".navbar-collapse").collapse("hide");
+	});
 
-  // Activate scrollspy to add active class to navbar items on scroll
-  $('body').scrollspy({
-    target: '#sideNav'
-  });
-
+	// Activate scrollspy to add active class to navbar items on scroll
+	$("body").scrollspy({
+		target: "#sideNav"
+	});
 })(jQuery); // End of use strict
 
 var TxtType = function(el, toRotate, period) {
-  this.toRotate = toRotate;
-  this.el = el;
-  this.loopNum = 0;
-  this.period = parseInt(period, 10) || 2000;
-  this.txt = '';
-  this.tick();
-  this.isDeleting = false;
+	this.toRotate = toRotate;
+	this.el = el;
+	this.loopNum = 0;
+	this.period = parseInt(period, 10) || 2000;
+	this.txt = "";
+	this.tick();
+	this.isDeleting = false;
 };
 
 TxtType.prototype.tick = function() {
-  var i = this.loopNum % this.toRotate.length;
-  var fullTxt = this.toRotate[i];
+	var i = this.loopNum % this.toRotate.length;
+	var fullTxt = this.toRotate[i];
 
-  if (this.isDeleting) {
-  this.txt = fullTxt.substring(0, this.txt.length - 1);
-  } else {
-  this.txt = fullTxt.substring(0, this.txt.length + 1);
-  }
+	if (this.isDeleting) {
+		this.txt = fullTxt.substring(0, this.txt.length - 1);
+	} else {
+		this.txt = fullTxt.substring(0, this.txt.length + 1);
+	}
 
-  this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+	this.el.innerHTML = '<span class="wrap">' + this.txt + "</span>";
 
-  var that = this;
-  var delta = 200 - Math.random() * 100;
+	var that = this;
+	var delta = 200 - Math.random() * 100;
 
-  if (this.isDeleting) { delta /= 2; }
+	if (this.isDeleting) {
+		delta /= 2;
+	}
 
-  if (!this.isDeleting && this.txt === fullTxt) {
-  delta = this.period * 2;
-  this.isDeleting = true;
-  } else if (this.isDeleting && this.txt === '') {
-  this.isDeleting = false;
-  this.loopNum++;
-  delta = 500;
-  }
+	if (!this.isDeleting && this.txt === fullTxt) {
+		delta = this.period * 2;
+		this.isDeleting = true;
+	} else if (this.isDeleting && this.txt === "") {
+		this.isDeleting = false;
+		this.loopNum++;
+		delta = 500;
+	}
 
-  setTimeout(function() {
-  that.tick();
-  }, delta);
+	setTimeout(function() {
+		that.tick();
+	}, delta);
 };
 
 window.onload = function() {
-  var elements = document.getElementsByClassName('typewrite');
-  for (var i=0; i<elements.length; i++) {
-      var toRotate = elements[i].getAttribute('data-type');
-      var period = elements[i].getAttribute('data-period');
-      if (toRotate) {
-        new TxtType(elements[i], JSON.parse(toRotate), period);
-      }
-  }
-
+	var elements = document.getElementsByClassName("typewrite");
+	for (var i = 0; i < elements.length; i++) {
+		var toRotate = elements[i].getAttribute("data-type");
+		var period = elements[i].getAttribute("data-period");
+		if (toRotate) {
+			new TxtType(elements[i], JSON.parse(toRotate), period);
+		}
+	}
 };
